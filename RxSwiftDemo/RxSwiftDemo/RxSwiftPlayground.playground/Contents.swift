c61bc824ffa9e43670b50fa83e361c63f405d939
Hiram Castro <hiramcastrodeveloper@gmail.com>
2021-12-28 22:32:13 -0800

    working on emmiting events using single

---
+++ RxSwiftDemo/RxSwiftDemo/RxSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+import RxSwift
+
+var greeting = "Hello, playground"
+
+_ = Observable.of("Hello RxSwift!")
---
+++ RxSwiftDemo/RxSwiftDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftDemo
+//
+//  Created by Hiram Castro on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
